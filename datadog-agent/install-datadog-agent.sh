867a3cd876cf87c40ca9487df2e4178befef0975
Jaeyo <jaeyo.developer@gmail.com>
2020-08-31 14:00:45 +0900

    collect pod labes as datadog tags

--- datadog-agent/install-datadog-agent.sh
+++ datadog-agent/install-datadog-agent.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # usage: ./install-datadog-agent.sh <API_KEY>
 
+if [ $# -eq 0 ]
+  then
+    echo "no api key provided. usage: ./install-datadog-agent.sh <API_KEY>"
+    exit
+fi
+
 helm install datadog-agent -f values.yaml  --set datadog.apiKey=$1 datadog/datadog --set targetSystem=linux
 
--- datadog-agent/upgrade-datadog-agent.sh
+++ datadog-agent/upgrade-datadog-agent.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # usage: ./upgrade-datadog-agent.sh <API_KEY>
 
+if [ $# -eq 0 ]
+  then
+    echo "no api key provided. usage: ./upgrade-datadog-agent.sh <API_KEY>"
+    exit
+fi
+
 helm upgrade -f values.yaml --set datadog.apiKey=$1 datadog-agent datadog/datadog
 
